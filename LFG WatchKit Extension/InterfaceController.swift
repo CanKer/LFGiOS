edc9968525ba24393940015453c24b8cb9a02135
CanKer <cdialike@gmail.com>
2015-11-30 05:26:59 -0600

    Initial Commit

---
+++ LFG WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  LFG WatchKit Extension
+//
+//  Created by Jorge Arturo César Martínez on 30/11/15.
+//  Copyright © 2015 Jorge Arturo César Martínez. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
